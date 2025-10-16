d09a58efbd53fa50f7bfc4bddeeedd4c1e8fabc5
Nguyễn Đình Thái <dinhthai0312@gmail.com>
2025-10-16 13:07:47 +0700

    add AccountMiddleware

--- thabicare_admin/base.py
+++ thabicare_admin/base.py
@@ -73,6 +73,9 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    
+    'allauth.account.middleware.AccountMiddleware',
+
 ]
 
 ROOT_URLCONF = 'thabicare_admin.urls'
